a06b89ffc70f77a44992f75b7519f5af7c36e2db
Ahmet ATILGAN <atilganajan@gmail.com>
2024-01-12 02:35:13 +0300

    add: Routes and Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SmsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/send-sms', [SmsController::class, 'sendSms']);
+Route::get('/sms-reports', [SmsController::class, 'getSmsReports']);
+Route::get('/sms-reports/{id}', [SmsController::class, 'getSmsReportDetail']);
+Route::get('/sms-reports/filter/{date}', [SmsController::class, 'filterSmsReportsByDate']);
+
+
+
